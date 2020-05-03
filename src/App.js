2e869947b75128ae8b3272a33b7ca69d18ea0843
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-05-03 16:35:20 -0700

    list page rendering some data from data file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import ListPage from './ListPage.js';
 import './App.css';
 
 
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className="PokemonApp">
+        <ListPage />
         
       </div>
     )
--- src/ListPage.js
+++ src/ListPage.js
@@ -1,10 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import data from './data.js';
 
 export default class ListPage extends Component {
+
+    state = {
+        data: data.results
+    }
     render() {
+        // console.log(this.state.data);
         return (
             <div>
-                
+               {
+                   this.state.data.map((character) => {
+                    return <div className="pokemon-card">
+                        {character.pokemon} is type 
+                        {character.type_1} with an attack of 
+                        {character.attack} and a defense of 
+                        {character.defense}
+                        <img alt="pokemon-character" src={character.url_image}/>
+                        </div>
+                   })
+               } 
             </div>
         )
     }
