396e71bb5b4947c9d11c06df431df8c95bddf167
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-05-03 18:02:35 -0700

    search bar tested with input logging to console

--- src/ListPage.js
+++ src/ListPage.js
@@ -7,16 +7,25 @@ export default class ListPage extends Component {
     state = {
         data: data.results
     }
+
+    handleChange= () => {
+        console.log('yo yo homes')
+    }
+
     render() {
         return (
             <div>
-               {
-                   this.state.data.map((character) => {
-                    return <div className="pokemon-card">
-                        <PokemonItem character={character}/>
-                        </div>
-                   })
-               } 
+                <input className="pokemon-search" onChange={this.handleChange}/>
+                <label>Search Pokemon!</label>
+                <ul>
+                    {
+                        this.state.data.map((character) => {
+                            return <div className="pokemon-card">
+                                <PokemonItem character={character}/>
+                                </div>
+                        })
+                    } 
+               </ul>
             </div>
         )
     }
--- src/PokemonItem.js
+++ src/PokemonItem.js
@@ -5,7 +5,7 @@ export default class PokemonItem extends Component {
         return (
             <ul>
                 <li className="pokemon-item">
-                    <h2 className="pokemon-name">{this.props.character.pokemon}</h2>
+                    <h2 className="pokemon-name">{this.props.character.pokemon.toUpperCase()}</h2>
                     <img className="pokemon-character" alt={this.props.character.pokemon} src={this.props.character.url_image}/>
                     <p className="pokemon-type">Type = {this.props.character.type_1}</p>
                     <p className="pokemon-attack">Attack = {this.props.character.attack}</p>
