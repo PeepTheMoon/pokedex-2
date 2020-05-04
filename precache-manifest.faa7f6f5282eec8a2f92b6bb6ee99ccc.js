9fab0c8b32206c27493105d7ff7e352bc6bab548
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-05-03 18:51:09 -0700

    Updates

---
+++ precache-manifest.faa7f6f5282eec8a2f92b6bb6ee99ccc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef3e00439fb27d9d31d25729c6494511",
+    "url": "/pokedex-2/index.html"
+  },
+  {
+    "revision": "c363e09ca32ff993c5ef",
+    "url": "/pokedex-2/static/css/main.33c0ee0b.chunk.css"
+  },
+  {
+    "revision": "1585381cb75fe3d14e06",
+    "url": "/pokedex-2/static/js/2.5e7f11ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex-2/static/js/2.5e7f11ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c363e09ca32ff993c5ef",
+    "url": "/pokedex-2/static/js/main.47dea5a4.chunk.js"
+  },
+  {
+    "revision": "faf0269f639126adac75",
+    "url": "/pokedex-2/static/js/runtime-main.7c9ecdd1.js"
+  }
+]);
