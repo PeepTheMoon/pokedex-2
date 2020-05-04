b30e36cad41376324f93e5f26568623cd1f23676
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-05-03 18:30:30 -0700

    added search bar that connects to the pokemon api and renders results to page

--- src/ListPage.js
+++ src/ListPage.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
-import data from './data.js';
+// import data from './data.js';
 import PokemonItem from './PokemonItem.js';
+import request from 'superagent';
 
 export default class ListPage extends Component {
 
     state = {
-        data: data.results
+        data: [],
+        searchQuery: ''
     }
 
-    handleChange= () => {
-        console.log('yo yo homes')
+    handleChange= (e) => {
+        const value = e.target.value;
+        this.setState({searchQuery: value});
+    }
+
+    handleClick = async () => {
+        const fetchedData = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?pokemon=${this.state.searchQuery}`);
+        console.log(fetchedData);
+        this.setState({ data: fetchedData.body.results });
     }
 
     render() {
         return (
             <div>
                 <input className="pokemon-search" onChange={this.handleChange}/>
-                <label>Search Pokemon!</label>
+                <button onClick={this.handleClick}>Search Pokemon!</button>
                 <ul>
                     {
                         this.state.data.map((character) => {
