73332ad4875d59c1599b9cf0626b8ac499c1b81b
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-05-06 00:50:53 -0700

    updates tests

--- src/PokemonItem.js
+++ src/PokemonItem.js
@@ -7,10 +7,17 @@ export default class PokemonItem extends Component {
                 <ul>
                     <li className={`pokemon-item ${this.props.character.type_1}`}>
                         <Link to={`/character/${this.props.character.pokemon}`}>
+
                         <h2 className="pokemon-name">{this.props.character.pokemon.toUpperCase()}</h2>
-                        <img className="pokemon-character" alt={this.props.character.pokemon} src={this.props.character.url_image}/> </Link>
+
+                        <img className="pokemon-character" alt={this.props.character.pokemon} src={this.props.character.url_image}/> 
+
+                        </Link>
+
                         <p className="pokemon-type">Type = {this.props.character.type_1}</p>
+
                         <p className="pokemon-attack">Attack = {this.props.character.attack}</p>
+
                         <p className="pokemon-defense">Defense ={this.props.character.defense}</p>
                     </li>
                 </ul>
--- src/questions.md
+++ src/questions.md
@@ -1,12 +1,16 @@
 Quetsions:
-url search bar -- text entered doesn'y populate search bar
 
-prev and next buttons no longer are working, and they're rendered twice on the page.  Not sure why
+-url search bar -- text entered doesn'y populate search bar
+
+-prev and next buttons no longer are working, and they're rendered twice on the page.  Not sure why
 
 
 To do:
-move searchbar code into it's own component with handleChange and this.props.handleChange
 
-in render, put a loading spinner
+-in render, put a loading spinner
 
-put pagination at top 
+-put pagination at top 
+
+-disable next button when on last page of results
+
+-update url search params to populate search bar when entered in browser
